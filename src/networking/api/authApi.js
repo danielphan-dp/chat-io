1fd06606185d6c0aaece7ed228ea8bdb319bebcd
danielphandptp <business.edwardp@gmail.com>
2022-07-20 10:28:31 -0400

    implemented basic structure of dashboard

--- src/networking/api/authApi.js
+++ src/networking/api/authApi.js
@@ -1,5 +1,21 @@
 import apiClient from './apiClient';
+import { logout } from '../../shared/utils/auth';
 
+apiClient.interceptors.request.use(
+  (config) => {
+    const userDetails = localStorage.getItem('user');
+    if (userDetails) {
+      const token = JSON.parse(userDetails).token;
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+// public routes
 export const register = async (data) => {
   try {
     return await apiClient.post('/auth/register', data);
@@ -19,5 +35,13 @@ export const login = async (data) => {
       error: true,
       exception,
     };
+  }
+};
+
+// secure routes
+export const checkResponseCode = (exception) => {
+  const responseCode = exception?.response?.status;
+  if (responseCode) {
+    (responseCode === 401 || responseCode === 403) && logout();
   }
 };
