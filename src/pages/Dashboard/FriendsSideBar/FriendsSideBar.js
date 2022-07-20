999735e01ba106db7f44634ace5aff3c9bb5e3b6
danielphandptp <business.edwardp@gmail.com>
2022-07-20 11:02:57 -0400

    added invitations section

--- src/pages/Dashboard/FriendsSideBar/FriendsSideBar.js
+++ src/pages/Dashboard/FriendsSideBar/FriendsSideBar.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { styled } from '@mui/system';
 
+import AddFriendButton from './AddFriendButton';
+import FriendsList from './FriendsList/FriendsList';
+import FriendsTitle from './FriendsTitle';
+import PendingInvitationsList from './PendingInvitationsList/PendingInvitationsList';
+
 const MainContainer = styled('div')({
   width: '224px',
   height: '100%',
@@ -11,7 +16,15 @@ const MainContainer = styled('div')({
 });
 
 const FriendsSideBar = () => {
-  return <MainContainer></MainContainer>;
+  return (
+    <MainContainer>
+      <AddFriendButton />
+      <FriendsTitle title="Private Messages" />
+      <FriendsList />
+      <FriendsTitle title="Invitations" />
+      <PendingInvitationsList />
+    </MainContainer>
+  );
 };
 
 export default FriendsSideBar;
--- src/pages/Dashboard/SideBar/MainPageButton.js
+++ src/pages/Dashboard/SideBar/MainPageButton.js
@@ -9,11 +9,12 @@ const MainPageButton = () => {
         width: '48px',
         height: '48px',
         borderRadius: '16px',
+        margin: 0,
         padding: 0,
         minWidth: 0,
         marginTop: '10px',
         color: 'white',
-        backgroundCOlor: '#5865F2',
+        backgroundColor: '#5865F2',
       }}
     >
       <GroupsIcon />
