5b12a39f3212a9da3c31e6e899b2861c21a560ad
danielphandptp <business.edwardp@gmail.com>
2022-07-20 11:54:55 -0400

    implemented add friend pop-up dialog with responsive send button

--- src/shared/utils/validators.js
+++ src/shared/utils/validators.js
@@ -1,15 +1,15 @@
-const validateMail = (mail) => {
+export const validateMail = (mail) => {
   const emailRegexPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   return emailRegexPattern.test(mail);
 };
 
-const validatePassword = (password) => {
+export const validatePassword = (password) => {
   const l = password.length;
   // TODO: perform extra checks if needed
   return 6 < l && l < 12;
 };
 
-const validateUsername = (username) => {
+export const validateUsername = (username) => {
   const l = username.length;
   // TODO: perform extra checks if needed
   return 2 < l && l < 13;
